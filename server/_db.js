35bbdfb54ca134acabac3435f145023e481b1084
Kate Humphrey <kate@fullstackacademy.com>
2017-09-07 13:14:42 -0400

    update port and db_uri for heroku

--- server/_db.js
+++ server/_db.js
@@ -2,7 +2,7 @@
 
 var Sequelize = require('sequelize');
 
-var databaseURI = 'postgres://localhost:5432/auther';
+var databaseURI = process.env.DATABASE_URI || 'postgres://localhost:5432/auther';
 
 var db = new Sequelize(databaseURI, {
   define: {
