d5f5d2e0e5ae146bbdf3b6c31fb1fd58a150a305
Derrick Beining <derrick.beining@gmail.com>
2017-09-09 00:53:25 -0400

    added authorization restrictions to api endpoints

--- server/api/users/user.router.js
+++ server/api/users/user.router.js
@@ -1,60 +1,71 @@
-'use strict';
+'use strict'
 
-var router = require('express').Router();
+var router = require('express').Router()
 
-var HttpError = require('../../utils/HttpError');
-var User = require('./user.model');
-var Story = require('../stories/story.model');
+var HttpError = require('../../utils/HttpError')
+var User = require('./user.model')
+var Story = require('../stories/story.model')
+const auth = require('../utils/authorizeRequest')
 
 router.param('id', function (req, res, next, id) {
   User.findById(id)
-  .then(function (user) {
-    if (!user) throw HttpError(404);
-    req.requestedUser = user;
-    next();
-    return null;
-  })
-  .catch(next);
-});
+    .then(function (user) {
+      if (!user) throw HttpError(404)
+      req.requestedUser = user
+      next()
+      return null
+    })
+    .catch(next)
+})
 
-router.get('/', function (req, res, next) {
-  User.findAll({})
-  .then(function (users) {
-    res.json(users);
+router.route('/')
+  .get(
+  function (req, res, next) {
+    User.findAll({})
+      .then(function (users) {
+        res.json(users)
+      })
+      .catch(next)
   })
-  .catch(next);
-});
 
-router.post('/', function (req, res, next) {
-  User.create(req.body)
-  .then(function (user) {
-    res.status(201).json(user);
+  .post(
+  auth.mustBeGuestOrAdmin,
+  function createNewUser (req, res, next) {
+    User.create(req.body)
+      .then(function (user) {
+        res.status(201).json(user)
+      })
+      .catch(next)
   })
-  .catch(next);
-});
 
-router.get('/:id', function (req, res, next) {
-  req.requestedUser.reload(User.options.scopes.populated())
-  .then(function (requestedUser) {
-    res.json(requestedUser);
+
+router.route('/:id')
+  .get(function (req, res, next) {
+    req.requestedUser.reload(User.options.scopes.populated())
+      .then(function (requestedUser) {
+        res.json(requestedUser)
+      })
+      .catch(next)
   })
-  .catch(next);
-});
 
-router.put('/:id', function (req, res, next) {
-  req.requestedUser.update(req.body)
-  .then(function (user) {
-    res.json(user);
+  .put(
+  auth.mustBeSelfOrAdmin,
+  function updateUser (req, res, next) {
+    req.requestedUser.update(req.body)
+      .then(function (user) {
+        res.json(user)
+      })
+      .catch(next)
   })
-  .catch(next);
-});
 
-router.delete('/:id', function (req, res, next) {
-  req.requestedUser.destroy()
-  .then(function () {
-    res.status(204).end();
+  .delete(
+  auth.mustBeSelfOrAdmin,
+  function (req, res, next) {
+    req.requestedUser.destroy()
+      .then(function () {
+        res.status(204).end()
+      })
+      .catch(next)
   })
-  .catch(next);
-});
 
-module.exports = router;
+module.exports = router
---
+++ server/api/utils/authorizeRequest.js
@@ -0,0 +1,87 @@
+const mustBeLoggedIn = (req, res, next) => {
+  if (!req.user) return res.sendStatus(401)
+  else next()
+}
+
+const mustBeSelfOrAdmin = (req, res, next) => {
+  if (!req.user) return res.sendStatus(401)
+  if (!req.user.isAdmin && req.user.id !== req.requestedUser.id) {
+    return res.sendStatus(403)
+  } else {
+    next();
+  }
+}
+
+const mustBeGuestOrAdmin = (req, res, next) => {
+  if (!req.user) return res.sendStatus(401)
+  if (!req.user.isAdmin) return res.sendStatus(403)
+  else next()
+}
+
+const mustBeOwnerOrAdmin = (req, res, next) => {
+  if (!req.user) return res.sendStatus(401)
+  if (!req.user.isAdmin && req.user.id !== req.story.author_id) {
+    return res.sendStatus(403)
+  } else {
+    next();
+  }
+}
+
+const mustBeAttributedAuthorOrAdmin = (req, res, next) => {
+  if (!req.user) return res.sendStatus(401)
+  if (!req.user.isAdmin && req.user.id !== req.body.author_id) {
+    res.status(403).end();
+  } else {
+    next();
+  }
+}
+
+function selfOrAdmin (req, res, next) {
+  if (!req.user) {
+    res.status(401).end();
+  } else if (!req.user.isAdmin && req.user.id !== req.requestedUser.id) {
+    res.status(403).end();
+  } else {
+    next();
+  }
+}
+
+function admin (req, res, next) {
+  // `req.user` was retrieved from postgres db via sequelize
+  if (!req.user) {
+    res.status(401).end();
+  } else if (!req.user.isAdmin) {
+    res.status(403).end();
+  } else {
+    next();
+  }
+}
+
+function adminOrAuthor (req, res, next) {
+  if (!req.user) {
+    res.status(401).end();
+  } else if (!req.user.isAdmin && req.user.id !== req.story.author_id) {
+    res.status(403).end();
+  } else {
+    next();
+  }
+}
+
+function adminOrCreatingAuthor (req, res, next) {
+  if (!req.user) {
+    res.status(401).end();
+  } else if (!req.user.isAdmin && req.user.id !== req.body.author_id) {
+    res.status(403).end();
+  } else {
+    next();
+  }
+}
+
+
+module.exports = {
+  mustBeLoggedIn,
+  mustBeSelfOrAdmin,
+  mustBeGuestOrAdmin,
+  mustBeOwnerOrAdmin,
+  mustBeAttributedAuthorOrAdmin
+};
